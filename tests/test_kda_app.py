8c72de83ecca711e3261de0af775bf54831c9c40
The Pratik <pratik.patel@emerson.com>
2018-11-29 17:47:07 -0600

    do not forget that inputs is list - dont understand why -pp

--- tests/test_kda_app.py
+++ tests/test_kda_app.py
@@ -140,7 +140,7 @@ class TestKinesisDataAnalyticsApp(unittest.TestCase):
 
         self.app.process_request()
 
-        self.app.client.create_application.assert_called_once_with(ApplicationName=mock.ANY, ApplicationDescription=mock.ANY, ApplicationCode=mock.ANY, Inputs=self.get_expected_input_configuration(), Outputs=mock.ANY, CloudWatchLoggingOptions=mock.ANY)
+        self.app.client.create_application.assert_called_once_with(ApplicationName=mock.ANY, ApplicationDescription=mock.ANY, ApplicationCode=mock.ANY, Inputs=[self.get_expected_input_configuration()], Outputs=mock.ANY, CloudWatchLoggingOptions=mock.ANY)
 
     def get_expected_input_configuration(self):
         expected = {
